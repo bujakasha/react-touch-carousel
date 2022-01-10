3cd0772a78ac7e8777792a68b1104b529806fd57
bujakasha <casperv96@gmail.com>
2022-01-10 09:39:07 +0200

    ...

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import TouchCarousel from './TouchCarousel'
+export {
+  range,
+  clamp,
+  precision,
+  getTouchPosition,
+  getTouchId,
+  omit,
+  modCursor
+} from './utils'
+
+export default TouchCarousel
